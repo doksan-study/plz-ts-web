afdeeacfe873deb010fc7da0cea9886b1dab9d7b
beom220 <beom220@gmail.com>
2022-10-09 15:01:26 +0900

    chore : provide set

--- src/index.tsx
+++ src/index.tsx
@@ -2,26 +2,50 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {ThemeProvider} from "@mui/system";
-import theme from "@/theme";
-import {CssBaseline} from "@mui/material";
 import {BrowserRouter} from "react-router-dom";
+import {RecoilRoot} from "recoil";
+import {QueryClientProvider, QueryClient} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+import {ThemeProvider} from "@mui/system";
+import {CssBaseline} from "@mui/material";
+import theme from "@/theme";
+import {HelmetProvider} from 'react-helmet-async';
+
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            retry: 0,
+        },
+    }
+})
+
 root.render(
     <React.StrictMode>
-        <ThemeProvider theme={theme}>
-            <CssBaseline/>
-            <BrowserRouter>
-                <App/>
-            </BrowserRouter>
-        </ThemeProvider>
+        <RecoilRoot>
+            <QueryClientProvider client={queryClient}>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline/>
+                    <BrowserRouter>
+                        <HelmetProvider>
+                            <App/>
+                        </HelmetProvider>
+                    </BrowserRouter>
+                </ThemeProvider>
+
+                {process.env.NODE_ENV === "development" && (
+                    <ReactQueryDevtools
+                        initialIsOpen={false}
+                        position={"bottom-right"}
+                    />
+                )}
+            </QueryClientProvider>
+        </RecoilRoot>
     </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
